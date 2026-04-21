3b3b5a4b543203fcde85f1ccf2fb260e87a10dce
Kumar Vedant <2023aib1009@iitrpr.ac.in>
2026-04-21 14:22:32 +0530

    Add TimescaleDB for storing gold table data using alembic.

---
+++ dashboard/backend/db/models.py
@@ -0,0 +1,43 @@
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import Double, Integer, Boolean, Date, Text
+from datetime import datetime, date as date_type
+import sqlalchemy as sa
+
+class Base(DeclarativeBase):
+    pass
+
+class Market(Base):
+    __tablename__ = "market"
+
+    open_time            : Mapped[datetime] = mapped_column(sa.TIMESTAMP(timezone=True), primary_key=True)
+    symbol               : Mapped[str]      = mapped_column(Text, primary_key=True)
+    open                 : Mapped[float]    = mapped_column(Double, nullable=True)
+    high                 : Mapped[float]    = mapped_column(Double, nullable=True)
+    low                  : Mapped[float]    = mapped_column(Double, nullable=True)
+    close                : Mapped[float]    = mapped_column(Double, nullable=True)
+    volume               : Mapped[float]    = mapped_column(Double, nullable=True)
+    trades               : Mapped[int]      = mapped_column(Integer, nullable=True)
+    taker_buy_base       : Mapped[float]    = mapped_column(Double, nullable=True)
+    log_return           : Mapped[float]    = mapped_column(Double, nullable=True)
+    volatility           : Mapped[float]    = mapped_column(Double, nullable=True)
+    imbalance_ratio      : Mapped[float]    = mapped_column(Double, nullable=True)
+    buy_ratio            : Mapped[float]    = mapped_column(Double, nullable=True)
+    log_return_lag1      : Mapped[float]    = mapped_column(Double, nullable=True)
+    log_return_lag2      : Mapped[float]    = mapped_column(Double, nullable=True)
+    buy_ratio_lag1       : Mapped[float]    = mapped_column(Double, nullable=True)
+    ma_5                 : Mapped[float]    = mapped_column(Double, nullable=True)
+    ma_20                : Mapped[float]    = mapped_column(Double, nullable=True)
+    volatility_5         : Mapped[float]    = mapped_column(Double, nullable=True)
+    volume_5             : Mapped[float]    = mapped_column(Double, nullable=True)
+    buy_ratio_5          : Mapped[float]    = mapped_column(Double, nullable=True)
+    momentum             : Mapped[float]    = mapped_column(Double, nullable=True)
+    volume_spike         : Mapped[float]    = mapped_column(Double, nullable=True)
+    price_range_ratio    : Mapped[float]    = mapped_column(Double, nullable=True)
+    body_size            : Mapped[float]    = mapped_column(Double, nullable=True)
+    hour                 : Mapped[int]      = mapped_column(Integer, nullable=True)
+    day_of_week          : Mapped[int]      = mapped_column(Integer, nullable=True)
+    trend_strength       : Mapped[float]    = mapped_column(Double, nullable=True)
+    volatility_ratio     : Mapped[float]    = mapped_column(Double, nullable=True)
+    is_valid_feature_row : Mapped[bool]     = mapped_column(Boolean, nullable=True)
+    date                 : Mapped[date_type]     = mapped_column(Date, nullable=True)
+    ingestion_time       : Mapped[datetime] = mapped_column(sa.TIMESTAMP(timezone=True), nullable=True)
